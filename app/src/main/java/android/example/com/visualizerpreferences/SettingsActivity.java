376190a9b85271c1cc79cbbdaf99187b40830eb1
Yar <yarsher.dev@gmail.com>
2019-02-12 11:36:12 -0500

    SettingsFragment

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -1,7 +1,10 @@
 package android.example.com.visualizerpreferences;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,8 +13,30 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
 
     }
+
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 
 
 }
